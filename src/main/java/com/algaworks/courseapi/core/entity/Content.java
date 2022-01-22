2b26e8e786017a64bba3e6be961f0c73fb8a60b6
Bruno <bruninhosudre@gmail.com>
2022-01-21 23:41:26 -0300

    [COURSE-API] Remove hibernate

--- src/main/java/com/algaworks/courseapi/core/entity/Content.java
+++ src/main/java/com/algaworks/courseapi/core/entity/Content.java
@@ -11,7 +11,6 @@ import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
